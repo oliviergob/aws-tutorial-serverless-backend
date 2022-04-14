e3a279198d7d3ea81fbbaba837b53914752c5a3a
Olivier Gobilliard <olivier.gob@gmail.com>
2022-04-14 22:51:24 +0100

    Adding config file and fixing SAM template

--- resources/functions/configuration/function.py
+++ resources/functions/configuration/function.py
@@ -1,9 +1,21 @@
 import boto3
+import yaml
 import json
+import os
 
+
+S3_BUCKET = os.environ['S3_BUCKET']
+CONFIG_FILE = 'config.yaml'
 
 def handler(event, context):
 
-    print('Hello World')
+    # Getting config file from S3 bucket
+    s3 = boto3.client('s3')
+    response = s3.get_object(Bucket=S3_BUCKET, Key=CONFIG_FILE)
+
+    # Parsing response file
+    configfile = yaml.safe_load(response["Body"])
+    
+    return(configfile)
 
 
